d3d08d8954a92bae18e7690b35415445be489411
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-12-14 22:39:58 +0700

    Automate verify about us testcase

--- cypress/support/page_objects/HomePage.js
+++ cypress/support/page_objects/HomePage.js
@@ -1,27 +1,8 @@
 import HomePageUI from "../page_interfaces/homePageUI";
+import CommonPage from "./CommonPage";
 
-class HomePage {
-    clickToSignInHeaderButton() {
-        cy.get(HomePageUI.SIGN_IN_HEADER_BUTTON).click();
-    }
-
-    enterToUserNameTextbox(username) {
-        cy.get(HomePageUI.SIGN_IN_USERNAME_TEXTBOX).type(username);
-    }
-
-    enterToPasswordTextbox(password) {
-        cy.get(HomePageUI.SIGN_IN_PASSWORD_TEXTBOX).type(password);
-    }
-
-    clickToSignUpButton() {
-        cy.xpath(HomePageUI.SIGN_UP_BUTTON).click();
-    }
-
-    verifySignInAlert(expectedText) {
-        cy.on('window:alert', (alertText) => {
-            expect(alertText).to.contains(expectedText);
-        })
-    }
+class HomePage extends CommonPage {
+    
 }
 
 export default HomePage;
